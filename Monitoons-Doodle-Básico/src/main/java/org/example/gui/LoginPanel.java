ea440a458df604f58b883c3e0a94a6fbadca2ec0
JPedroDuarte23 <joao.dsilva@sptech.school>
2023-10-25 21:11:15 -0300

    Colocando logo

--- Monitoons-Doodle-Básico/src/main/java/org/example/gui/LoginPanel.java
+++ Monitoons-Doodle-Básico/src/main/java/org/example/gui/LoginPanel.java
@@ -17,8 +17,8 @@ public class LoginPanel extends TelaPanel {
 
     public LoginPanel(JPanel telas, JFrame janela) {
         super(telas, janela);
-
-        ImageIcon imagemIcon = new ImageIcon("images/logo2.png");
+//;
+        ImageIcon imagemIcon = new ImageIcon(getClass().getResource("/assets/logo2.png"));
         JLabel imagemLabel = new JLabel(imagemIcon);
         imagemLabel.setBounds(440, 105, 300, 100);
         JLabel txtLogin = new JLabel("Insira seu Email:");
